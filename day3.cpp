75e0252d12028ce8e0a7d2073ef0c94a82f69dac
Chris Hartman <chris.m.hartman@gmail.com>
2019-12-03 10:35:09 -0900

    Clean up day 3

--- day3.cpp
+++ day3.cpp
@@ -1,145 +1,49 @@
-//
-// Created by Chris Hartman on 12/2/19.
-//
-#include "Intcode.hpp"
-
 #include <algorithm>
-#include <cmath>
-#include <cstdlib>
 #include <fstream>
-#include <iomanip>
 #include <iostream>
 #include <map>
-#include <numeric>
-#include <queue>
-#include <set>
 #include <sstream>
-#include <string>
-#include <tuple>
 #include <utility>
-#include <vector>
-using std::abs;
-using std::ceil;
-using std::cout;
-using std::endl;
-using std::forward_as_tuple;
-using std::ifstream;
-using std::istream;
-using std::istringstream;
-using std::map;
-using std::max;
-using std::max_element;
-using std::min;
-using std::pair;
-using std::queue;
-using std::set;
-using std::sqrt;
-using std::string;
-using std::swap;
-using std::tie;
-using std::tuple;
-using std::vector;
 
-void day3() {
-  auto star1 = 999999999;
-  auto star2 = 0;
-  ifstream ifile("../day3.txt");
-  //  Intcode    prog(ifile);
-  // istringstream ifile("R75,D30,R83,U83,L12,D49,R71,U7,L72\n"
-  //                      "U62,R66,U55,R34,D71,R55,D58,R83");
-  string line;
-  getline(ifile, line);
-  string line2;
-  getline(ifile, line2);
-  istringstream in1(line);
-  map<pair<int, int>, int> m;
-  auto x = 0;
-  auto y = 0;
-  auto steps = 0;
-  while (in1) {
-    string s;
-    char c;
-    char _c;
-    int i;
-    //    istringstream iline(line);
-    in1 >> c >> i >> _c;
-    cout << c << i << endl;
-    switch (c) {
-    case 'U':
-      for (int n = 0; n < i; ++n) {
-        ++y;
-        m[{x, y}] = 1;
-      }
-      break;
-    case 'D':
-      for (int n = 0; n < i; ++n) {
-        --y;
-        m[{x, y}] = 1;
-      }
-      break;
-    case 'L':
-      for (int n = 0; n < i; ++n) {
-        --x;
-        m[{x, y}] = 1;
-      }
-      break;
-    case 'R':
-      for (int n = 0; n < i; ++n) {
-        ++x;
-        m[{x, y}] = 1;
-      }
-      break;
+using Position = std::pair<int, int>;
+
+std::map<Position, int> pathToGrid(const std::string &path) {
+  static std::map<char, Position> dirs = {
+      {'U', {0, 1}}, {'D', {0, -1}}, {'L', {-1, 0}}, {'R', {1, 0}}};
+
+  std::map<Position, int> out;
+
+  std::istringstream in(path);
+  char dir, comma;
+  int steps;
+  int posX = 0, posY = 0;
+  int step = 0;
+  while (in >> dir >> steps >> comma)
+    for (int s = 0; s < steps; ++s) {
+      posX += dirs[dir].first;
+      posY += dirs[dir].second;
+      out[{posX, posY}] = ++step;
     }
-    cout << x << " " << y << endl;
-  }
-  istringstream in2(line2);
-  cout << "------\n";
-  x = y = 0;
-  while (in2) {
-
-    string s;
-    char c;
-    char _c;
-    int i;
-    //    istringstream iline(line);
-    in2 >> c >> i >> _c;
-    cout << c << i << endl;
-    switch (c) {
-    case 'U':
-      for (int n = 0; n < i; ++n) {
-        ++y;
-        if (m[{x, y}] == 1)
-          star1 = min(star1, abs(x) + abs(y));
-      }
-      break;
-    case 'D':
-      for (int n = 0; n < i; ++n) {
-        --y;
-        if (m[{x, y}] == 1)
-          star1 = min(star1, abs(x) + abs(y));
-      }
-      break;
-    case 'L':
-      for (int n = 0; n < i; ++n) {
-        --x;
-        if (m[{x, y}] == 1)
-          star1 = min(star1, abs(x) + abs(y));
-      }
-      break;
-    case 'R':
-      for (int n = 0; n < i; ++n) {
-        ++x;
-        if (m[{x, y}] == 1)
-          star1 = min(star1, abs(x) + abs(y));
-      }
-      break;
-    }
-    cout << x << " " << y << endl;
-  }
-
-  cout << "Day 3 star 1 = " << star1 << "\n";
-  cout << "Day 3 star 2 = " << star2 << "\n";
+  return out;
 }
 
-// day 1 calculate fuel required
-// day 2 run IntCode program
+void day3() {
+  std::ifstream ifile("../day3.txt");
+  std::string line1, line2;
+  ifile >> line1 >> line2;
+
+  auto grid1 = pathToGrid(line1);
+  auto grid2 = pathToGrid(line2);
+  int star1 = grid1.size() + grid2.size(); // larger than any possible result
+  int star2 = grid1.size() + grid2.size(); // larger than any possible result
+
+  for (auto [pos, steps] : grid2) {
+    if (grid1.count(pos) > 0) { // path intersection
+      star1 = std::min(star1, abs(pos.first) + abs(pos.second));
+      star2 = std::min(star2, grid1[pos] + grid2[pos]);
+    }
+  }
+
+  std::cout << "Day 3 star 1 = " << star1 << "\n";
+  std::cout << "Day 3 star 2 = " << star2 << "\n";
+}
