02613c0fe0f51fc85ff9971247d617bc9306db77
Chris Hartman <chris.m.hartman@gmail.com>
2019-12-03 21:16:07 -0900

    Finish day 4

--- day4.cpp
+++ day4.cpp
@@ -1,58 +1,31 @@
 //
 // Created by Chris Hartman on 12/3/19.
 //
-
-#include "Intcode.hpp"
-
 #include <algorithm>
-#include <cmath>
-#include <cstdlib>
-#include <fstream>
-#include <iomanip>
 #include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
 #include <sstream>
-#include <string>
-#include <tuple>
-#include <utility>
 #include <vector>
-using std::abs;
-using std::ceil;
-using std::cout;
-using std::endl;
-using std::forward_as_tuple;
-using std::ifstream;
-using std::istream;
-using std::istringstream;
-using std::map;
-using std::max;
-using std::max_element;
-using std::min;
-using std::pair;
-using std::queue;
-using std::set;
-using std::sqrt;
-using std::string;
-using std::swap;
-using std::tie;
-using std::tuple;
-using std::vector;
-
 void day4() {
   auto star1 = 0;
   auto star2 = 0;
-  ifstream ifile("../day4.txt");
-  Intcode i(ifile);
-  string line;
-  while (getline(ifile, line)) {
-    string _s;
-    istringstream iline(line);
-    iline >> _s;
+
+  for (int i = 353096; i <= 843212; ++i) {
+    std::ostringstream digitsStream;
+    digitsStream << i;
+    const std::string &digits = digitsStream.str();
+    if (!std::is_sorted(digits.begin(), digits.end()))
+      continue;
+    auto numTimes = [&](auto d) {
+      return std::count(digits.begin(), digits.end(), d);
+    };
+    if (std::any_of(digits.begin(), digits.end(),
+                    [&](auto d) { return numTimes(d) >= 2; }))
+      ++star1;
+    if (std::any_of(digits.begin(), digits.end(),
+                    [&](auto d) { return numTimes(d) == 2; }))
+      ++star2;
   }
 
-  cout << "Day 4 star 1 = " << star1 << "\n";
-  cout << "Day 4 star 2 = " << star2 << "\n";
+  std::cout << "Day 4 star 1 = " << star1 << "\n";
+  std::cout << "Day 4 star 2 = " << star2 << "\n";
 }
