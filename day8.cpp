2ae66f2f909b6f95ee2b214814737811574c43c3
Chris Hartman <chris.m.hartman@gmail.com>
2019-12-08 07:40:17 -0900

    Shorter day 8

--- day8.cpp
+++ day8.cpp
@@ -2,27 +2,22 @@
 #include <iostream>
 #include <numeric>
 #include <string>
-#include <vector>
 
 void day8() {
   auto star1 = 0;
   std::ifstream ifile("../day8.txt");
-  std::vector<std::string> pix;
   std::string layer(6 * 25, ' ');
+  std::string star2(6 * 25, '2');
   auto minZeroes = layer.size();
   while (ifile.read(layer.data(), 6 * 25)) {
-    pix.push_back(layer);
     if (auto z = std::count(layer.begin(), layer.end(), '0'); z < minZeroes) {
       minZeroes = z;
       star1 = std::count(layer.begin(), layer.end(), '1') *
               std::count(layer.begin(), layer.end(), '2');
     }
-  }
-
-  std::string star2 = pix.front();
-  for (const auto &theLayer : pix)
     for (auto i = 0ul; i < star2.size(); ++i)
-      star2[i] = (star2[i] != '2' ? star2[i] : theLayer[i]);
+      star2[i] = (star2[i] != '2' ? star2[i] : layer[i]);
+  }
 
   std::cout << "Day 8 star 1 = " << star1 << "\nDay 8 star 2 =";
 
