a251e547b22f6a58ee38b7f5a3aa08370dab7062
Chris Hartman <chris.m.hartman@gmail.com>
2019-12-20 15:09:19 -0900

    Clean Day 20.

--- day20.cpp
+++ day20.cpp
@@ -1,252 +1,113 @@
-//
-// Created by Chris Hartman on 12/19/19.
-//
-#include "Intcode.hpp"
-
-#include <algorithm>
 #include <cctype>
-#include <cmath>
-#include <cstdlib>
 #include <fstream>
-#include <iomanip>
 #include <iostream>
-#include <iterator>
 #include <map>
-#include <memory>
-#include <numeric>
 #include <queue>
 #include <set>
-#include <sstream>
 #include <string>
-#include <tuple>
 #include <utility>
 #include <vector>
-using std::abs;
-using std::ceil;
-using std::cout;
-using std::endl;
-using std::forward_as_tuple;
-using std::ifstream;
-using std::istream;
-using std::istringstream;
-using std::map;
-using std::pair;
-using std::queue;
-using std::set;
-using std::string;
-using std::tuple;
-using std::vector;
 
-using Vertex = pair<int, int>;
-using RVertex = pair<Vertex, int>;
-map<Vertex, vector<RVertex>> edges;
-map<string, vector<Vertex>> portals;
-
-auto findChoices() {
-  std::ofstream out("out.txt");
-  set<RVertex> visited;
-  map<RVertex, RVertex> parent;
-  queue<pair<RVertex, int>> q; // node, distance
-  q.push({{portals["AA"].front(), 0}, 0});
-  while (!q.empty()) {
-    auto [node, dist] = q.front();
-    auto node2 = node;
-    q.pop();
-    if (node == RVertex{portals["ZZ"].front(), 0}) {
-      cout << dist << endl;
-      break;
-    }
-    if (visited.count(node))
-      continue;           // don't visit a node we've already seen
-    visited.insert(node); // mark this node as visited
-    auto [loc, lvl] = node;
-    auto loc2 = loc;
-    auto lvl2 = lvl;
-    for (auto [nbr, dLevel] : edges[loc]) {
-      auto nbr2 = nbr;
-      auto dLevel2 = dLevel;
-      if (lvl + dLevel >= 0 && !visited.count({nbr, lvl + dLevel})) {
-        q.push({{nbr, lvl + dLevel}, dist + 1}); // search neighbor nodes
-        parent[{nbr, lvl + dLevel}] = node;
-        //        out << "(" << loc.first << "," << loc.second << ")" << lvl <<
-        //        " ----> "; out << "(" << nbr.first << "," << nbr.second << ")"
-        //        << lvl + dLevel
-        //            << endl;
-      }
-    }
-  }
-
-  auto node = RVertex{portals["ZZ"].front(), 0};
-  while (node != RVertex{portals["AA"].front(), 0}) {
-    auto [loc, lvl] = node;
-    out << "(" << loc.first << "," << loc.second << ")" << lvl << endl;
-    node = parent[node];
-  }
-}
-
-void day20() {
-  auto star1 = 0;
-  auto star2 = 0;
-  ifstream ifile("../day20.txt");
-  //    istringstream ifile("         A           \n"
-  //                        "         A           \n"
-  //                        "  #######.#########  \n"
-  //                        "  #######.........#  \n"
-  //                        "  #######.#######.#  \n"
-  //                        "  #######.#######.#  \n"
-  //                        "  #######.#######.#  \n"
-  //                        "  #####  B    ###.#  \n"
-  //                        "BC...##  C    ###.#  \n"
-  //                        "  ##.##       ###.#  \n"
-  //                        "  ##...DE  F  ###.#  \n"
-  //                        "  #####    G  ###.#  \n"
-  //                        "  #########.#####.#  \n"
-  //                        "DE..#######...###.#  \n"
-  //                        "  #.#########.###.#  \n"
-  //                        "FG..#########.....#  \n"
-  //                        "  ###########.#####  \n"
-  //                        "             Z       \n"
-  //                        "             Z       ");
-
-  //  istringstream ifile("             Z L X W       C                 \n"
-  //                      "             Z P Q B       K                 \n"
-  //                      "  ###########.#.#.#.#######.###############  \n"
-  //                      "  #...#.......#.#.......#.#.......#.#.#...#  \n"
-  //                      "  ###.#.#.#.#.#.#.#.###.#.#.#######.#.#.###  \n"
-  //                      "  #.#...#.#.#...#.#.#...#...#...#.#.......#  \n"
-  //                      "  #.###.#######.###.###.#.###.###.#.#######  \n"
-  //                      "  #...#.......#.#...#...#.............#...#  \n"
-  //                      "  #.#########.#######.#.#######.#######.###  \n"
-  //                      "  #...#.#    F       R I       Z    #.#.#.#  \n"
-  //                      "  #.###.#    D       E C       H    #.#.#.#  \n"
-  //                      "  #.#...#                           #...#.#  \n"
-  //                      "  #.###.#                           #.###.#  \n"
-  //                      "  #.#....OA                       WB..#.#..ZH\n"
-  //                      "  #.###.#                           #.#.#.#  \n"
-  //                      "CJ......#                           #.....#  \n"
-  //                      "  #######                           #######  \n"
-  //                      "  #.#....CK                         #......IC\n"
-  //                      "  #.###.#                           #.###.#  \n"
-  //                      "  #.....#                           #...#.#  \n"
-  //                      "  ###.###                           #.#.#.#  \n"
-  //                      "XF....#.#                         RF..#.#.#  \n"
-  //                      "  #####.#                           #######  \n"
-  //                      "  #......CJ                       NM..#...#  \n"
-  //                      "  ###.#.#                           #.###.#  \n"
-  //                      "RE....#.#                           #......RF\n"
-  //                      "  ###.###        X   X       L      #.#.#.#  \n"
-  //                      "  #.....#        F   Q       P      #.#.#.#  \n"
-  //                      "  ###.###########.###.#######.#########.###  \n"
-  //                      "  #.....#...#.....#.......#...#.....#.#...#  \n"
-  //                      "  #####.#.###.#######.#######.###.###.#.#.#  \n"
-  //                      "  #.......#.......#.#.#.#.#...#...#...#.#.#  \n"
-  //                      "  #####.###.#####.#.#.#.#.###.###.#.###.###  \n"
-  //                      "  #.......#.....#.#...#...............#...#  \n"
-  //                      "  #############.#.#.###.###################  \n"
-  //                      "               A O F   N                     \n"
-  //                      "               A A D   M                     ");
-
-  int outC, inC, outR, inR;
-
-  string line;
-  vector<string> grid;
-  while (getline(ifile, line)) {
-    grid.push_back(line);
-  }
-
-  for (int r = 0; r < grid.size(); ++r)
-    for (int c = 0; c < grid[r].size(); ++c) {
-      auto ch = grid[r][c];
-      if (ch != '.')
-        continue;
-      auto putEdge = [&](int y, int x) {
-        if (grid[y][x] == '.')
-          edges[{r, c}].push_back({{y, x}, 0});
-      };
-      putEdge(r - 1, c);
-      putEdge(r + 1, c);
-      putEdge(r, c + 1);
-      putEdge(r, c - 1);
+class Day20 {
+  using Vertex = std::pair<int, int>;
+  using RVertex = std::pair<Vertex, int>; // Vertex and recursion level
+public:
+  Day20(const std::string &filename) {
+    std::ifstream ifile(filename);
+    std::string line;
+    while (getline(ifile, line)) {
+      grid.push_back(line);
     }
 
-  for (int r = 0; r < grid.size(); ++r)
-    for (int c = 0; c < grid[r].size(); ++c) {
-      auto ch = grid[r][c];
-      if (isupper(ch)) {
-        string name;
-        name += ch;
-        if (r + 1 < grid.size() && isupper(grid[r + 1][c])) {
-          name += grid[r + 1][c];
-          //            cout << name;
-          if (r + 2 < grid.size() && grid[r + 2][c] == '.') {
+    findPortals();
+    makeEdges();
+  }
+  int star1() { return distanceOfPath(false); }
+  int star2() { return distanceOfPath(true); }
 
-            portals[name].push_back({r + 2, c});
-            cout << r + 2 << "," << c << endl;
-          } else {
-            portals[name].push_back({r - 1, c});
-            cout << r - 1 << "," << c << endl;
-          }
-        } else if (c + 1 < grid[r].size() && isupper(grid[r][c + 1])) {
-          name += grid[r][c + 1];
-          cout << name;
-          if (c + 2 < grid[r].size() && grid[r][c + 2] == '.') {
-            portals[name].push_back({r, c + 2});
-            cout << r << "," << c + 2 << endl;
-          } else {
-            portals[name].push_back({r, c - 1});
-            cout << r << "," << c - 1 << endl;
+private:
+  bool onOutside(int r, int c) {
+    return (r == 2 || r == grid.size() - 3 || c == 2 || c == grid.size() - 3);
+  }
+
+  void findPortals() {
+    for (int r = 0; r < grid.size(); ++r)
+      for (int c = 0; c < grid[r].size(); ++c) {
+        auto ch = grid[r][c];
+        if (isupper(ch)) {
+          std::string name;
+          name += ch;
+          if (r + 1 < grid.size() && isupper(grid[r + 1][c])) {
+            name += grid[r + 1][c];
+            if (r + 2 < grid.size() && grid[r + 2][c] == '.')
+              portals[name].push_back({r + 2, c});
+            else
+              portals[name].push_back({r - 1, c});
+          } else if (c + 1 < grid[r].size() && isupper(grid[r][c + 1])) {
+            name += grid[r][c + 1];
+            if (c + 2 < grid[r].size() && grid[r][c + 2] == '.')
+              portals[name].push_back({r, c + 2});
+            else
+              portals[name].push_back({r, c - 1});
           }
         }
       }
-    }
+  }
 
-  for (auto [name, vs] : portals) {
-    if (name == "AA" || name == "ZZ")
-      continue;
-    int first;
-    {
-      auto [r1, c1] = vs.front();
-      int dLevel = 0;
-      if (r1 == 2 || r1 == grid.size() - 3 || c1 == 2 ||
-          c1 == grid[0].size() - 3)
-        dLevel = -1; // outer to inner
-      else
-        dLevel = 1;
-      edges[vs.front()].push_back({vs.back(), dLevel});
-      first = dLevel;
-    }
-    {
-      auto [r1, c1] = vs.back();
-      int dLevel = 0;
-      if (r1 == 2 || r1 == grid.size() - 3 || c1 == 2 ||
-          c1 == grid[0].size() - 3)
-        dLevel = -1; // outer to inner
-      else
-        dLevel = 1;
-      edges[vs.back()].push_back({vs.front(), dLevel});
-      assert(first != dLevel);
+  void makeEdges() {
+    for (int r = 0; r < grid.size(); ++r)
+      for (int c = 0; c < grid[r].size(); ++c) {
+        if (grid[r][c] != '.')
+          continue;
+        if (grid[r - 1][c] == '.')
+          edges[{r, c}].push_back({{r - 1, c}, 0});
+        if (grid[r + 1][c] == '.')
+          edges[{r, c}].push_back({{r + 1, c}, 0});
+        if (grid[r][c - 1] == '.')
+          edges[{r, c}].push_back({{r, c - 1}, 0});
+        if (grid[r][c + 1] == '.')
+          edges[{r, c}].push_back({{r, c + 1}, 0});
+      }
+
+    for (auto [name, locations] : portals) {
+      if (name == "AA" || name == "ZZ")
+        continue;
+      auto [r, c] = locations.front();
+      int dLevel = onOutside(r, c) ? -1 : 1;
+      edges[locations.front()].push_back({locations.back(), dLevel});
+      edges[locations.back()].push_back({locations.front(), -dLevel});
     }
   }
 
-  for (auto [p, es] : edges) {
-    cout << p.first << "," << p.second << " --> ";
-    for (auto [e, lvl] : es)
-      cout << e.first << "," << e.second << "(" << lvl << ")   ";
-    cout << endl;
+  int distanceOfPath(bool recursiveMaze) {
+    std::set<RVertex> visited;
+    std::queue<std::pair<RVertex, int>> q; // node, distance
+    q.push({RVertex{portals["AA"].front(), 0}, 0});
+    while (!q.empty()) {
+      auto [node, dist] = q.front();
+      q.pop();
+      if (visited.count(node))
+        continue;
+      if (node == RVertex{portals["ZZ"].front(), 0})
+        return dist;
+      visited.insert(node);
+      auto [nodePos, nodeLvl] = node;
+      for (auto [nbr, dLevel] : edges.at(nodePos)) {
+        auto newLevel = recursiveMaze ? nodeLvl + dLevel : 0;
+        if (!visited.count({nbr, newLevel}) && newLevel >= 0)
+          q.push({{nbr, newLevel}, dist + 1});
+      }
+    }
+    return -1;
   }
 
-  for (auto [name, es] : portals) {
-    if (name == "AA" || name == "ZZ")
-      continue;
-    cout << name << " --> ";
-    for (auto e : es)
-      cout << e.first << "," << e.second << "   ";
-    cout << endl;
-  }
+  std::vector<std::string> grid;
+  std::map<Vertex, std::vector<RVertex>> edges;
+  std::map<std::string, std::vector<Vertex>> portals;
+};
 
-  findChoices();
-
-  cout << grid.size() << " " << grid[0].size();
-  cout << "Day 20 star 1 = " << star1 << "\n";
-  cout << "Day 20 star 2 = " << star2 << "\n";
-} // not 1306
+void day20() {
+  Day20 day20("../day20.txt");
+  std::cout << "Day 20 star 1 = " << day20.star1() << "\n";
+  std::cout << "Day 20 star 2 = " << day20.star2() << "\n";
+}
