ad731629def9572da218e9be7be3e4afd3084cf1
Chris Hartman <chris.m.hartman@gmail.com>
2019-12-22 08:10:09 -0900

    Day 22 star 1 numerically

--- day22.cpp
+++ day22.cpp
@@ -38,16 +38,47 @@ using std::tie;
 using std::tuple;
 using std::vector;
 
-void dealIncrement(vector<int> &v, int increment) {
-  auto w = v;
-  for (int i = 0; i < v.size(); ++i)
-    w[(i * increment) % v.size()] = v[i];
-  v.swap(w);
+using Number = long long;
+
+Number numCards = 119'315'717'514'047;
+Number numTimes = 101'741'582'076'661;
+
+void xgcd(Number *result, Number a, Number b) {
+  Number aa[2] = {1, 0}, bb[2] = {0, 1}, q;
+  while (true) {
+    q = a / b;
+    a = a % b;
+    aa[0] = aa[0] - q * aa[1];
+    bb[0] = bb[0] - q * bb[1];
+    if (a == 0) {
+      result[0] = b;
+      result[1] = aa[1];
+      result[2] = bb[1];
+      return;
+    };
+    q = b / a;
+    b = b % a;
+    aa[1] = aa[1] - q * aa[0];
+    bb[1] = bb[1] - q * bb[0];
+    if (b == 0) {
+      result[0] = a;
+      result[1] = aa[0];
+      result[2] = bb[0];
+      return;
+    };
+  };
+}
+
+Number inverse(Number x) {
+  Number e[3];
+  xgcd(e, x, numCards);
+  return e[1];
 }
 
 void day22() {
-  auto star2 = 0;
+  numCards = 10007;
   ifstream ifile("../day22.txt");
+
   //  istringstream ifile("deal with increment 7\n"
   //                      "deal into new stack\n"
   //                      "deal into new stack\n");
@@ -64,18 +95,23 @@ void day22() {
   //                      "deal with increment 3\n"
   //                      "cut -1\n");
   //                      "Result: 9 2 5 8 1 4 7 0 3 6");
-  vector<int> v(10007);
-  iota(v.begin(), v.end(), 0);
 
-  string line;
-  while (getline(ifile, line)) {
+  string aline;
+  vector<string> shuffle;
+  while (getline(ifile, aline)) {
+    shuffle.push_back(aline);
+  }
+
+  Number mul = 1, add = 0;
+  for (const auto &line : shuffle) {
     string _s;
     istringstream iline(line);
     iline >> _s;
     if (_s == "cut") {
       int i;
       iline >> i;
-      std::rotate(v.begin(), v.begin() + (i + v.size()) % v.size(), v.end());
+      add += (numCards - i);
+      add %= numCards;
     } else {
       assert(_s == "deal");
       iline >> _s;
@@ -83,22 +119,51 @@ void day22() {
         int i;
         iline >> _s >> i;
         assert(_s == "increment");
-        dealIncrement(v, i);
+        mul = mul * i % numCards;
+        add = add * i % numCards;
       } else {
         assert(_s == "into");
-        std::reverse(v.begin(), v.end());
+        mul = numCards - mul;
+        add = numCards - 1 - add;
       }
     }
   }
 
-  //  for(auto i:v)
-  //    cout << i << " ";
-  //  cout << endl;
-
-  auto star1 = std::find(v.begin(), v.end(), 2019) - v.begin();
+  auto star1 = (2019 * mul + add) % numCards;
   cout << "Day 22 star 1 = " << star1 << "\n";
+
+  mul = 1;
+  add = 0;
+  for (auto t = shuffle.rbegin(); t != shuffle.rend(); ++t) {
+    const auto &line = *t;
+    string _s;
+    istringstream iline(line);
+    iline >> _s;
+    if (_s == "cut") {
+      int i;
+      iline >> i;
+      add += i;
+      add %= numCards;
+    } else {
+      assert(_s == "deal");
+      iline >> _s;
+      if (_s == "with") {
+        int i;
+        iline >> _s >> i;
+        assert(_s == "increment");
+        mul = mul * inverse(i) % numCards;
+        add = add * inverse(i) % numCards;
+      } else {
+        assert(_s == "into");
+        mul = numCards - mul;
+        add = numCards - 1 - add;
+      }
+    }
+  }
+  mul = (mul + numCards) % numCards;
+  auto star2 = (4086 * mul + add) % numCards;
   cout << "Day 22 star 2 = " << star2 << "\n";
-} // 2808 is too low
+}
 
 // cut -135
 // deal with increment 38
